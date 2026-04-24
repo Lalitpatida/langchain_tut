8462914dc2eb90b0240e687473c29d9402f8b653
Lalit <lalpatidar@bestpeers.com>
2026-04-24 12:53:01 +0530

    initial

---
+++ Chatmodels/hugging_face_model.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import ChatHuggingFace ,HuggingFacePipeline
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke('what is the capital of india')
+print(result)
